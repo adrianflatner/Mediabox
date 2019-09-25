afe795bb683bc3374b25ac7a568f8f3fa428cfc9
Adrian <adrianrflatner@gmail.com>
2019-09-25 18:20:08 +0200

    Ajax fetch now working

--- project2/src/components/Images.js
+++ project2/src/components/Images.js
@@ -1,32 +1,48 @@
 import React, { Component } from "react";
-import rich1 from "./pictures/richard1.svg";
-import rich2 from "./pictures/richard2.svg";
-import rich3 from "./pictures/richard3.svg";
-import trump1 from "./pictures/trump1.svg";
-import trump2 from "./pictures/trump2.svg";
-import trump3 from "./pictures/trump3.svg";
-import jaden1 from "./pictures/jaden1.svg";
-import jaden2 from "./pictures/jaden2.svg";
-import jaden3 from "./pictures/jaden3.svg";
-import michael1 from "./pictures/michael1.svg";
-import michael2 from "./pictures/michael2.svg";
-import michael3 from "./pictures/michael3.svg";
 
 const imagesPath = [
-  [trump1, trump2, trump3],
-  [rich1, rich2, rich3],
-  [jaden1, jaden2, jaden3],
-  [michael1, michael2, michael3]
+  ["trump1.svg", "trump2.svg", "trump3.svg"],
+  ["richard1.svg", "richard2.svg", "richard3.svg"],
+  ["jaden1.svg", "jaden2.svg", "jaden3.svg"],
+  ["michael1.svg","michael2.svg", "michael3.svg"]
 ];
 
+
 class Images extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      picture: null
+    };
+  }
+
+  componentDidMount(){
+    this.fetchImage()
+  }
+
+  fetchImage = () => {
+    var x = this.props.activeTab
+    var y = this.props.activePicture
+    fetch(`pictures/${imagesPath[x][y]}` )
+    .then(res => res.text())
+    .then((picture) => {
+      this.setState({
+        picture: picture
+      })
+    });
+  }
+
   render() {
     return (
       <div>
-        <img src={imagesPath[this.props.activeTab][this.props.activePicture]} />
+        {this.fetchImage()}
+        <div dangerouslySetInnerHTML={{ __html: this.state.picture}} />
       </div>
     );
   }
+
 }
 
 export default Images;
+
