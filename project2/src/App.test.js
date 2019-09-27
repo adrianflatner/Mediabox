96b89044613c4031517dfb7a48735e62a93fb55f
Adrian <adrianrflatner@gmail.com>
2019-09-27 17:15:28 +0200

    testing

--- project2/src/App.test.js
+++ project2/src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+	const div = document.createElement('div');
+	ReactDOM.render(<App />, div);
+	ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('App', () => {
+	test('loads without error', () => {
+		let element = <App />;
+
+		expect(element).toBeTruthy();
+	});
+});
+
+describe('App', () => {
+	test('render matches snapshot', () => {
+		const component = renderer.create(<App />);
+		const tree = component.toJSON();
+
+		expect(tree).toMatchSnapshot();
+	});
 });
